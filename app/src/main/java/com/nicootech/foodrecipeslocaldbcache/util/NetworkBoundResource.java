ac7f3b8f02d10948df3d8911822a58e53214900e
Ethan Rahimian <nicootech@gmail.com>
2021-06-30 10:24:29 -0700

    Network Bound Resource (Part 2/3)

--- app/src/main/java/com/nicootech/foodrecipeslocaldbcache/util/NetworkBoundResource.java
+++ app/src/main/java/com/nicootech/foodrecipeslocaldbcache/util/NetworkBoundResource.java
@@ -1,5 +1,8 @@
 package com.nicootech.foodrecipeslocaldbcache.util;
 
+import android.icu.text.CaseMap;
+
+import com.nicootech.foodrecipeslocaldbcache.AppExecutors;
 import com.nicootech.foodrecipeslocaldbcache.requests.responses.ApiResponse;
 
 import androidx.annotation.MainThread;
@@ -8,12 +11,55 @@ import androidx.annotation.Nullable;
 import androidx.annotation.WorkerThread;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MediatorLiveData;
+import androidx.lifecycle.Observer;
 
 // CacheObject: Type for the Resource data. (database cache)
 // RequestObject: Type for the API response. (network request)
 public abstract class NetworkBoundResource<CacheObject, RequestObject> {
 
+    private AppExecutors appExecutors;
     private MediatorLiveData<Resource<CacheObject>> results = new MediatorLiveData<>();
+
+    public NetworkBoundResource(AppExecutors appExecutors) {
+        this.appExecutors = appExecutors;
+        init();
+    }
+
+    private void init(){
+
+        //update liveData for loading status
+        results.setValue((Resource<CacheObject>)Resource.loading(null));
+
+        // observe LiveData source from local db
+        final LiveData<CacheObject> dbSource = loadFromDb();
+
+        results.addSource(dbSource, new Observer<CacheObject>() {
+            @Override
+            public void onChanged(CacheObject cacheObject) {
+                results.removeSource(dbSource);
+
+                if(shouldFetch(cacheObject)){
+                    //get data from the network
+                }
+                else{
+                    results.addSource(dbSource, new Observer<CacheObject>() {
+                        @Override
+                        public void onChanged(CacheObject cacheObject) {
+                            setValue(Resource.success(cacheObject));
+                        }
+                    });
+                }
+            }
+        });
+
+    }
+
+    private void setValue(Resource<CacheObject> newValue){
+        if(results.getValue() != newValue){
+            results.setValue(newValue);
+        }
+
+    }
 
     // Called to save the result of the API response into the database.
     @WorkerThread
