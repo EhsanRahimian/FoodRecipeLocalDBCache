27f580961f3ebc3d8c2ee3f38cd6e5b8a7f14379
Ethan Rahimian <nicootech@gmail.com>
2021-07-06 13:13:45 +0900

    Room Type Converters

--- app/src/main/java/com/nicootech/foodrecipeslocaldbcache/persistence/RecipeDatabase.java
+++ app/src/main/java/com/nicootech/foodrecipeslocaldbcache/persistence/RecipeDatabase.java
@@ -3,11 +3,14 @@ package com.nicootech.foodrecipeslocaldbcache.persistence;
 import android.content.Context;
 
 import com.nicootech.foodrecipeslocaldbcache.models.Recipe;
-
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
+
+
 @Database(entities = {Recipe.class}, version = 1)
+@TypeConverters({Converters.class})
 public abstract class RecipeDatabase extends RoomDatabase{
 
     public static final String DATABASE_NAME = "recipes_db";
